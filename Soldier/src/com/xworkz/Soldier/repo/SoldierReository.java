0fcd28340576a85194e1d6327e7ffd9a1cd0f2f3
Naveen T <112047838+naveen74111@users.noreply.github.com>
2023-01-27 09:33:07 +0530

    delete

--- Soldier/src/com/xworkz/Soldier/repo/SoldierReository.java
+++
@@ -1,8 +0,0 @@
-package com.xworkz.Soldier.repo;
-
-import com.xworkz.Soldier.dto.SoldierDTO;
-
-public interface SoldierReository {
-
-	boolean save(SoldierDTO dto);
-}
--- Soldier/src/com/xworkz/Soldier/service/SoldierService.java
+++
@@ -1,8 +0,0 @@
-package com.xworkz.Soldier.service;
-
-import com.xworkz.Soldier.dto.SoldierDTO;
-
-public interface SoldierService {
-
-	public boolean containAndValidation(SoldierDTO dto);
-}
